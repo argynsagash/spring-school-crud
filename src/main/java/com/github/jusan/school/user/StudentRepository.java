e37cdc26af48de63313f508aa0b13e67041abf36
Argyn Sagash <argyn.sagash@gmail.com>
2022-06-13 18:56:46 +0600

    Create authority

--- src/main/java/com/github/jusan/school/user/StudentRepository.java
+++ src/main/java/com/github/jusan/school/user/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.github.jusan.school.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Long> {
     Student findStudentByEmail(String email);
+
 }
